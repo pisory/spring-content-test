db164b1928ccbea06df5b8d48b842f9a233a3c1d
pisory <32595419+plzla@users.noreply.github.com>
2019-10-20 23:16:59 +0800

    commit

--- src/main/java/com/spring/autowired/aop/AOPTest.java
+++ src/main/java/com/spring/autowired/aop/AOPTest.java
@@ -1,6 +1,5 @@
 package com.spring.autowired.aop;
 
-import com.spring.autowired.aop.dao.DaoFactory;
 import com.spring.autowired.aop.dao.UserDao;
 import com.spring.autowired.aop.proxy.FactoryInvocationHandler;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -8,9 +7,9 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import java.lang.reflect.Proxy;
 
 /**
- * 使用AOP，来模拟一个mybatis
+ * 使用动态代理，来模拟一个mybatis
  * @author dada
- * @date 2019/10/9
+ * @date 2019/10/20
  * @description
  */
 public class AOPTest {
--- src/main/java/com/spring/autowired/aop/proxy/FactoryInvocationHandler.java
+++ src/main/java/com/spring/autowired/aop/proxy/FactoryInvocationHandler.java
@@ -7,16 +7,17 @@ import java.lang.reflect.Method;
 
 /**
  * @author dada
- * @date 2019/10/13
+ * @date 2019/10/20
  * @description
  */
 public class FactoryInvocationHandler implements InvocationHandler {
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-        //代理对象处理
         //connect db
         System.out.println("connect db");
         Select annotation = method.getAnnotation(Select.class);
+
+        //执行sql
         String sql = annotation.value()[0];
         System.out.println(sql);
         return null;
